b4dcf5d34fb61a534e4c5071ff032832c2314c11
lukaasm <lukaasm@gmail.com>
2022-11-29 14:46:33 +0100

    - fix autoexec

--- weaponized_towers/lua/tower_player_weapon_autoexec.lua
+++ weaponized_towers/lua/tower_player_weapon_autoexec.lua
@@ -1,8 +1,3 @@
--- requires game executable update for auto unlock
-if not RegisterGlobalEvent then
-    return
-end
-
-RegisterGlobalEvent("PlayerCreatedEvent", function(arg)
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(arg)
     BuildingService:UnlockBuilding("buildings/defense/tower_player_weapon")
 end)
